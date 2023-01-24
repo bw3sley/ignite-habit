202ce3a991f64b6a8b0a57e5769045bbb87d6fc8
Wesley Bernardes <bw3sley@gmail.com>
2023-01-23 22:02:09 -0300

    :sparkles: feat: create mobile app

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,21 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "./src/lib/dayjs";
+
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold
+} from "@expo-google-fonts/inter";
+
+import { Loading } from './src/components/Loading';
+
+import { Routes } from './src/routes';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold
+  })
 
-      <StatusBar style="auto" />
-    </View>
+  if(!fontsLoaded) {
+    return (
+      <Loading />
+    )
+  }
+
+  return (
+    <>
+      <Routes />
+      
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
